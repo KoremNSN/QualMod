006e1fb872a3adb3e048ff2f7b2ab2b25de5b58a
KoremNSN <40990514+KoremNSN@users.noreply.github.com>
2024-10-25 16:20:58 -0400

    Delete RunningScript directory

--- RunningScript/getTextDims.m
+++
@@ -1,6 +0,0 @@
-%% Function to get text dimensions
-function textDims = getTextDims(win,myText,textSize)
-Screen('TextSize',win,textSize);
-textRect = Screen ('TextBounds',win,myText);
-textDims = [textRect(3) textRect(4)]; % text width and height
-end
